ab93dc7442adcef98d5578960111368471fbddc0
Hamza Samad <160117284+Hamzasamad55@users.noreply.github.com>
2024-05-29 17:14:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+#! usr/bin/env node
+// types in typescript
+// number,boolean,string
+let userId = 102030;
+console.log(userId);
+let myFacebookAccountPassword = 111222;
+console.log(myFacebookAccountPassword);
+export {};
---
+++ index.ts
@@ -0,0 +1,12 @@
+#! usr/bin/env node
+
+// types in typescript
+// number,boolean,string
+
+let userId: number = 102030;
+
+console.log(userId);
+
+let myFacebookAccountPassword = 111222;
+
+console.log(myFacebookAccountPassword);
